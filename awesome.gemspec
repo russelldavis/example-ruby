0c4449fe1873bc09f0fb8f998e15240f5bf8719d
stevepeak <steve@stevepeak.net>
2014-10-17 12:07:27 +0000

    using .simplecov

--- awesome.gemspec
+++ awesome.gemspec
@@ -17,6 +17,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "simplecov"
   s.add_development_dependency "mocha"
   s.add_development_dependency "rake"
-  s.add_development_dependency "codecov"
 
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,11 @@
-require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter "/test/"
-end
-require 'codecov'
-if ENV['CI'] == 'true'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
+# require 'simplecov'
+# SimpleCov.start 'rails' do
+#   add_filter "/test/"
+# end
+# require 'codecov'
+# if ENV['CI'] == 'true'
+#   SimpleCov.formatter = SimpleCov::Formatter::Codecov
+# end
 
 require 'test/unit'
 require 'mocha/setup'
